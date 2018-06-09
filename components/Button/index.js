3fa73ea7bd432ad1fa89bc1d81b51f6c3a0ac935
Eugene Ross <e@eugeneross.com>
2018-06-09 16:53:34 -0700

    prettier test

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,42 +1,31 @@
 // Libraries
 import React, { Component } from 'react'
 import Link from 'next/link'
-
-// Styles
-import styles from './index.sass'
+import StyledButton from './styles'
 
 export default class Button extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    componentDidMount() {
+  componentDidMount() {}
 
-    }
-
-    // <Button 
-    //     className=' additional classnames '
-    //     href=' use this prop if link is outbound '
-    //     link=' use this prop if link is internal'
-    //     text=' button text '
-    // />
-
-    render() {
-        return (
-            this.props.href
-                ? (
-                    <a className={`${styles.btn} ${this.props.className}`} target='_blank' href={this.props.href}>
-                        {this.props.text}
-                    </a>
-                ) : (
-                    <Link href={this.props.link}>
-                        <a className={`${styles.btn} ${this.props.className}`}>
-                            {this.props.text}
-                        </a>
-                    </Link>
-                )
-        )
-    }
+  render() {
+    return this.props.href ? (
+      <StyledButton target="_blank" href={this.props.href}>
+        {this.props.text}
+      </StyledButton>
+    ) : (
+      <Link href={this.props.link}>
+        <StyledButton outline>{this.props.text}</StyledButton>
+      </Link>
+    )
+  }
 }
 
-
+// <Button
+//     className=' additional classnames '
+//     href=' use this prop if link is outbound '
+//     link=' use this prop if link is internal'
+//     text=' button text '
+// />
