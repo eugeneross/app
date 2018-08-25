44fa30508eb17812bcb6b168ca780b41509f0763
Dhanesh Kapadiya <11155587+dhanesh-kapadiya@users.noreply.github.com>
2018-08-25 22:53:37 +0530

    add sample use of tablet media query

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@
 import React, { Component } from 'react'
 
 import stylesheet from '../styles/index.sass'
-
+import styled from 'styled-components'
 
 import Nav from '../components/Nav'
+import { media } from '../utils'
 
-import { Button } from 'tortuga-lib'
+// import { Button } from 'tortuga-lib'
 
 export default class Index extends Component {
   constructor(props) {
@@ -14,17 +15,26 @@ export default class Index extends Component {
   }
 
   componentDidMount() {
-    console.log('Index Component mounted.')
+    console.log('Index Component mounted.', media)
   }
   render() {
     return (
       <div>
         <main>
 
-          index
-    
+          <MyDiv>index</MyDiv>
+
         </main>
       </div>
     )
   }
 }
+
+const MyDiv = styled.div`
+  color: white;
+  background: gray;
+  ${media.tablet`
+    background: yellow;
+    color: black;
+  `}
+`
